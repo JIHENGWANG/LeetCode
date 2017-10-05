23cea96b91e1ae04727436e67e9a7edbf627d452
jihengwang <jiw367@ucsd.edu>
2017-10-05 15:02:02 -0700

    adding linkedlist problems

---
+++ src/practiceHard/MergekSortedLists.java
@@ -0,0 +1,10 @@
+package practiceHard;
+
+public class MergekSortedLists {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/practiceMedium/RemoveNthNodeFromEndofList.java
@@ -0,0 +1,10 @@
+package practiceMedium;
+
+public class RemoveNthNodeFromEndofList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
